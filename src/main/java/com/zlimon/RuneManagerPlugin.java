612d89dd25313e5d9789b7956852fe40b4dffef0
Zlimon <simon@lonnestad.no>
2024-09-04 18:39:05 +0200

    Initial plugin

--- src/main/java/com/zlimon/RuneManagerPlugin.java
+++ src/main/java/com/zlimon/RuneManagerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.zlimon;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "RuneManager"
 )
-public class ExamplePlugin extends Plugin
+public class RuneManagerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private RuneManagerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	RuneManagerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(RuneManagerConfig.class);
 	}
 }
--- src/test/java/com/zlimon/RuneManagerPluginTest.java
+++ src/test/java/com/zlimon/RuneManagerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.zlimon;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RuneManagerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RuneManagerPlugin.class);
 		RuneLite.main(args);
 	}
 }
